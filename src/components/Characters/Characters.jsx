dc99b83badff9104ffaa4f86526d75b60bc3384b
Your Name <you@example.com>
2023-11-03 17:05:31 +0100

    Characters - list of characters styled

--- src/components/Characters/Characters.jsx
+++ src/components/Characters/Characters.jsx
@@ -12,10 +12,14 @@ export default function Characters() {
   } = useCharacters();
 
   const charactersList = characters?.map(character => (
-    <li key={character.id}>
-      <img src={character.image} alt={character.name} />
+    <li className='character-card' key={character.id}>
+      <div className='character-image'>
+        <img src={character.image} alt={character.name} />
+      </div>
       <h2>{character.name}</h2>
-      <p>{character.species}</p>
+      <ul>
+        <li className='character-species'>{character.species}</li>
+      </ul>
       <p>{character.origin.name}</p>
     </li>
   ));
--- src/components/Characters/Styled-component.tsx
+++ src/components/Characters/Styled-component.tsx
@@ -26,4 +26,64 @@ export const CharactersStyle = styled.main`
       display: none;
     }
   }
+
+  .list-of-characters {
+    width: 85vw;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+
+    .character-card {
+      margin: 0.8rem 0.6rem;
+      width: 14rem;
+      height: 16.5rem;
+      background-color: #0d7c85;
+      border: solid #c1da7e;
+      border-radius: 0.3rem;
+      position: relative;
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-star;
+      align-items: center;
+
+      .character-image {
+        height: 7.5rem;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        img {
+          height: 8rem;
+          border-radius: 4rem;
+          position: absolute;
+          top: -1.2rem;
+        }
+      }
+
+      h2 {
+        font-size: 1.3rem;
+        margin: 0 0.5rem;
+      }
+
+      .character-species {
+        font-size: 1rem;
+        font-weight: 200;
+      }
+
+      .character-species::before {
+        content: '⬤';
+        color: #5a9f1d;
+        display: inline-block;
+        width: 1em;
+        margin-left: -1em;
+      }
+
+      p {
+        font-size: 1rem;
+        margin: 0 0.5rem;
+      }
+    }
+  }
 `;
